71c0d5e8a50221cf992889e1ee2e533e965d0a17
Jobayer Hossan <jobayerhossan34@gmail.com>
2026-03-18 03:02:05 +0600

    Demo Code if functions.php

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /**
  * Theme setup
- */
+ */ 
 function djs_theme_setup() {
 
 	// Make theme available for translation.
@@ -188,7 +188,21 @@ function djs_enqueue_assets() {
 }
 add_action( 'wp_enqueue_scripts', 'djs_enqueue_assets' );
 
+/**
+ * Display a simple dashboard notice for theme admins.
+ */
+function djs_dashboard_demo_notice() {
+	if ( ! current_user_can( 'manage_options' ) ) {
+		return;
+	}
+	?>
+	<div class="notice notice-success is-dismissible">
+		<p><?php esc_html_e( 'Welcome to DJS theme. Your dashboard demo message is now active.', 'djs' ); ?></p>
+	</div>
+	<?php
+}
+add_action( 'admin_notices', 'djs_dashboard_demo_notice' );
+
 
 require_once get_template_directory() . '/inc/woocommerce.php';
 require_once get_template_directory() . '/inc/product-filter.php';
-
